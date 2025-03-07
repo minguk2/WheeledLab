9bc8fc08be21ad12fc1214a1e86aac3677d371a2
Alistair Keiller <alistair@keiller.net>
2025-03-06 22:49:05 -0800

    update for gymnasium v1.0

--- source/wheeledlab_rl/wheeledlab_rl/utils/custom_video_recorder.py
+++ source/wheeledlab_rl/wheeledlab_rl/utils/custom_video_recorder.py
@@ -1,73 +1,13 @@
-
 import os
-import os.path
-import av
-import cv2
 from typing import Callable
-import wandb
 
 import gymnasium as gym
-
-from gymnasium.wrappers.record_video import RecordVideo
-from gymnasium.wrappers.monitoring.video_recorder import VideoRecorder
-
-class CustomVideoRecorder(VideoRecorder):
-    """Overrides the close method to write videos to wandb."""
-    def __init__(self, **kargs):
-        self.video_resolution = kargs.pop('video_resolution')
-        self.wandb = kargs.pop('enable_wandb')
-        self.video_crf = kargs.pop('video_crf')
-        super().__init__(**kargs)
-
-    def resize_frames(self, frames, resolution):
-        """Resize frames to the given resolution."""
-        nW, nH = resolution
-        H, W = frames[0].shape[:2]
-        aspect_ratio = W / H
-        if aspect_ratio != nW / nH:
-            nH = round(nW / aspect_ratio)
-        return [cv2.resize(frame, (nW, nH)) for frame in frames]
-
-    def close(self):
-        """Flush all data to disk and close any open frame encoders."""
-        if not self.enabled or self._closed:
-            return
-
-        # Close the encoder
-        if len(self.recorded_frames) > 0:
-            recorded_frames = self.resize_frames(self.recorded_frames, self.video_resolution)
-            H, W = recorded_frames[0].shape[:2]
-            output = av.open(self.path, 'w')
-            output_stream = output.add_stream('libx264', rate=round(self.frames_per_sec))
-            output_stream.width = W
-            output_stream.height = H
-            output_stream.pix_fmt = "yuv444p"
-            output_stream.options = {"crf": str(self.video_crf), "preset": "veryslow"}
-            for frame in recorded_frames:
-                packet = output_stream.encode(av.VideoFrame.from_ndarray(frame, format='rgb24'))
-                output.mux(packet)
-            packet = output_stream.encode(None)
-            output.mux(packet)
-            output.close()
-
-            # log video to wandb
-            if self.wandb:
-                wandb.log({"Video": wandb.Video(self.path)}, commit=False)
-            self.recorded_frames, self.recorded_history = [], []
-        else:
-            # No frames captured. Set metadata.
-            if self.metadata is None:
-                self.metadata = {}
-            self.metadata["empty"] = True
-
-        self.write_metadata()
-
-        # Stop tracking this for autoclose
-        self._closed = True
+import wandb
+from gymnasium import error, logger
+from gymnasium.wrappers.rendering import RecordVideo
 
 
 class CustomRecordVideo(RecordVideo):
-
     def __init__(
         self,
         env: gym.Env,
@@ -78,36 +18,42 @@ class CustomRecordVideo(RecordVideo):
         name_prefix: str = "rl-video",
         disable_logger: bool = False,
         enable_wandb: bool = True,
-        video_resolution: tuple[int, int] = (1280, 720),
-        video_crf: int = 30,
     ):
         if enable_wandb and wandb.run.name is None:
             raise ValueError("wandb must be initialized before wrapping.")
 
-        super().__init__(env, video_folder, episode_trigger,
-                         step_trigger, video_length, name_prefix, disable_logger)
-        self.enable_wandb = enable_wandb
-        self.video_resolution = video_resolution
-        self.video_crf = video_crf
-
-    def start_video_recorder(self):
-        """Starts video recorder using :class:`video_recorder.VideoRecorder`."""
-        self.close_video_recorder()
-
-        video_name = f"{self.name_prefix}-step-{self.step_id}"
-        if self.episode_trigger:
-            video_name = f"{self.name_prefix}-episode-{self.episode_id}"
-
-        base_path = os.path.join(self.video_folder, video_name)
-        self.video_recorder = CustomVideoRecorder(
-            env=self.env,
-            base_path=base_path,
-            metadata={"step_id": self.step_id, "episode_id": self.episode_id},
-            disable_logger=self.disable_logger,
-            enable_wandb=self.enable_wandb,
-            video_resolution=self.video_resolution,
-            video_crf=self.video_crf,
+        super().__init__(
+            env,
+            video_folder,
+            episode_trigger,
+            step_trigger,
+            video_length,
+            name_prefix,
+            disable_logger,
         )
-        self.video_recorder.capture_frame()
-        self.recorded_frames = 1
-        self.recording = True
+        self.enable_wandb = enable_wandb
+
+    def stop_recording(self):
+        """Stop current recording and saves the video."""
+        assert self.recording, "stop_recording was called, but no recording was started"
+
+        if len(self.recorded_frames) == 0:
+            logger.warn("Ignored saving a video as there were zero frames to save.")
+        else:
+            try:
+                from moviepy.video.io.ImageSequenceClip import ImageSequenceClip
+            except ImportError as e:
+                raise error.DependencyNotInstalled(
+                    'MoviePy is not installed, run `pip install "gymnasium[other]"`'
+                ) from e
+
+            clip = ImageSequenceClip(self.recorded_frames, fps=60)
+            moviepy_logger = None if self.disable_logger else "bar"
+            path = os.path.join(self.video_folder, f"{self._video_name}.mp4")
+            clip.write_videofile(path, logger=moviepy_logger)
+            if self.enable_wandb:
+                wandb.log({"Video": wandb.Video(path)}, commit=False)
+
+        self.recorded_frames = []
+        self.recording = False
+        self._video_name = None
