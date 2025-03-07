72b8272ceb9b96076ff7c801a75f504b310dad7b
Alistair Keiller <alistair@keiller.net>
2025-03-06 23:42:59 -0800

    use veryslow preset

--- source/wheeledlab_rl/wheeledlab_rl/utils/custom_video_recorder.py
+++ source/wheeledlab_rl/wheeledlab_rl/utils/custom_video_recorder.py
@@ -50,7 +50,7 @@ class CustomRecordVideo(RecordVideo):
             clip = ImageSequenceClip(self.recorded_frames, fps=60)
             moviepy_logger = None if self.disable_logger else "bar"
             path = os.path.join(self.video_folder, f"{self._video_name}.mp4")
-            clip.write_videofile(path, logger=moviepy_logger)
+            clip.write_videofile(path, logger=moviepy_logger, preset="veryslow")
             if self.enable_wandb:
                 wandb.log({"Video": wandb.Video(path)}, commit=False)
 
